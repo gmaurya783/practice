f31f0e1a368c0aa79b48d1da4b9a51cda8865d04
Gaurav <gmaurya783@gmail.com>
2021-11-12 09:46:33 +0530

    Fibbonacci series done

--- fibbonacci.c
+++ fibbonacci.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 
-int main(void)
-{
-    int n1=0, n2=1, n3, n;
-    printf("Enter the number ");
-    scanf("%d", &n);
+void print_Fibb(int);
+void print_Fibb_recur(int);
 
+void print_Fibb(int n)
+{
+    int n1=0, n2=1, n3;
     printf("Fibbonacci series: %d, %d", n1, n2);
     for(int i=2;i<n;i++)
     {
@@ -14,8 +14,31 @@ int main(void)
         n1= n2;
         n2= n3;
     }
+}
 
+void print_Fibb_recur(int n)
+{
+    static int n1= 0, n2= 1, n3;
+    if(n==0)
+    {
+        printf("\nFibbonacci series by recursion: %d, %d", n1, n2);
+    }
+    else
+    {
+        print_Fibb_recur(n-1);
+        n3 = n2+n1;
+        n1 = n2;
+        n2= n3;
+        printf(", %d", n3);
+    }
+}
 
-
+int main(void)
+{
+    int n;
+    printf("Enter the number ");
+    scanf("%d", &n);
+    print_Fibb(n);
+    print_Fibb_recur(n-2);
     return 0;
 }
